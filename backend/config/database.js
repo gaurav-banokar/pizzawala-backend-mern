12387e36b35f41e9f38eb295ab6f375410db5acf
Gaurav Dilip Banokar <gauravbanokar7@gmail.com>
2023-05-30 13:45:02 +0530

    add catch block in db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import ErrorHandler from "../utils/ErrorHandler";
 
 export const connectDB = async () => {
-    
- const { connection } = await mongoose.connect(process.env.MONGO_URI);
-
- console.log(`Database is connected to ${connection.host} `)
+    try {
+        const { connection } = await mongoose.connect(process.env.MONGO_URI);
+       
+        console.log(`Database is connected to ${connection.host} `)
+        
+    } catch (error) {
+        return new ErrorHandler("Database connection failed",502)
+    }
 }
